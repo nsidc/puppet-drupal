4438d1b426cbd47c1f9ac82517039bcf6524b493
Jeff Braucher <jeffrey.braucher@colorado.edu>
2015-04-07 22:50:51 -0600

    Setting up test config for module development

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,15 +1,2 @@
 # Load modules and classes
 hiera_include('classes')
-
-# Mount the drupal apps share
-include nsidc_nfs
-nsidc_nfs::sharemount{'/apps/drupal':
-  project => 'apps',
-  share => 'drupal'
-}
-
-# Trust the SSL certs for snowldap and iceldap
-drupal::ssl::trustcert{'snowldap.colorado.edu': port => 636 }
-drupal::ssl::trustcert{'iceldap.colorado.edu':  port => 636 }
-
-include drupal::apache::ssis
