be7f32b4eb63fd1bb15c21bbe534d9c0071da617
Jeff Braucher <jeff@braucher.net>
2015-11-04 09:18:21 -0700

    Fixed osfamily fact in spec for unit testing.

--- spec/classes/drupal_spec.rb
+++ spec/classes/drupal_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'drupal' do
 
-  let(:facts) { { :operatingsystem => 'Ubuntu' } }
+  let(:facts) { { :operatingsystem => 'Ubuntu', :osfamily => 'Debian' } }
 
   it { should compile.with_all_deps }
 
